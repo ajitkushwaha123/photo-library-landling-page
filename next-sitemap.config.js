4e65888d249e94b82a531db52cb43555388a8d93
ajitkushwaha123 <ajitkushwahaswe@gmail.com>
2025-08-17 13:38:10 +0530

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,38 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
-
 module.exports = {
-  siteUrl: "https://foodsnap.in",
-  changefreq: "daily",
-  priority: 0.7,
-  sitemapSize: 5000,
+  siteUrl: "https://foodsnap.in", 
   generateRobotsTxt: true, 
-  alternateRefs: [
-    {
-      href: "https://es.foodsnap.in",
-      hreflang: "es",
-    },
-    {
-      href: "https://fr.foodsnap.in",
-      hreflang: "fr",
-    },
-  ],
-
-  transform: async (config, path) => ({
-    loc: path,
-    changefreq: config.changefreq,
-    priority: config.priority,
-    lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-    alternateRefs: config.alternateRefs ?? [],
-  }),
-
-  additionalPaths: async () => [], 
-
+  sitemapSize: 5000, 
+  changefreq: "daily", 
+  priority: 0.7, 
   robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: "*",
-        allow: "/", 
-      },
-    ],
+    policies: [{ userAgent: "*", allow: "/" }],
   },
 };
